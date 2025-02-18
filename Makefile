0bba1cb3c13ec448bb65a8d7761017406801d4c2
Aspi100 <aspi.jb.ru@gmail.com>
2025-02-18 10:44:08 +0300

    handler init structure

--- Makefile
+++ Makefile
@@ -1,11 +1,6 @@
 POSTGRES_DSN := "postgres://postgres:postgres@localhost:5432/finapi?sslmode=disable"
-M := ""
 
 .PHONY: run
 run:
 	go run ./cmd/aviapi/main.go
 
-.PHONY: commit
-commit:
-	git add . && \
-	git commit -m "${M}"
--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -7,22 +7,28 @@ import (
 )
 
 type AirTicket struct {
-	From             string    `db:"from"`
-	To               string    `db:"to"`
-	Order            uuid.UUID `db:"order_id"`
-	Carrier          string    `db:"carrier"`
-	DepartureDate    time.Time `db:"departure_date"`
-	ArrivalDate      time.Time `db:"arrival_date"`
-	RegistrationDate time.Time `db:"registration_date"`
+	From             string    `db:"from" json:"from"`
+	To               string    `db:"to" json:"to"`
+	Order            uuid.UUID `db:"order_id" json:"order_id"`
+	Carrier          string    `db:"carrier" json:"carrier"`
+	DepartureDate    time.Time `db:"departure_date" json:"departure_date"`
+	ArrivalDate      time.Time `db:"arrival_date" json:"arrival_date"`
+	RegistrationDate time.Time `db:"registration_date" json:"registration_date"`
 }
 
 type Passenger struct {
-	FirstName  string `db:"first_name"`
-	LastName   string `db:"last_name"`
-	Patronymic string `db:"patronymic"`
+	FirstName  string `db:"first_name" json:"first_name"`
+	LastName   string `db:"last_name" json:"last_name"`
+	Patronymic string `db:"patronymic" json:"patronymic"`
 }
 
 type Document struct {
-	Type string    `db:"type"`
-	ID   uuid.UUID `db:"id"`
+	Type string    `db:"type" json:"type"`
+	ID   uuid.UUID `db:"id" json:"id"`
+}
+
+type FullInfo struct {
+	Ticket        AirTicket
+	PassengerList []Passenger
+	DocumentList  []Document
 }
